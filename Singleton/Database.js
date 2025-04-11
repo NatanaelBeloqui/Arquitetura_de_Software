f5739b50f7185c1c3be844ca0f60092c4d8f2279
Natanael <nbdb2005@gmail.com>
2025-04-10 21:20:19 -0300

    Padrão Singleton em desenvolvimento

---
+++ Singleton/Database.js
@@ -0,0 +1,24 @@
+class Database {
+    constructor() {
+        if (Database.instance) {
+            return Database.instance; // Impede multiplas instâncias
+        }
+        console.log('Criando uma nova conexão com o DB...');
+        this.connection = this.#connect();
+        Database.instance = this;
+    }
+
+    #connect() {
+        // Simula conexão com o BD
+        return {
+            status: 'Conectado';
+            timestamp: new Date();
+        };
+    }
+
+    getConnection() {
+        return this.connection;
+    }
+}
+
+module.exports = new Database(); // Exporta sempre a mesma instância
